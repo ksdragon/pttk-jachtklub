545c9770ecfc6946965d58a224fd08c27f4cb013
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-09-09 07:24:01 +0200

    add logo topbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+import { TopbarComponent } from './header/topbar/topbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './header/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule
