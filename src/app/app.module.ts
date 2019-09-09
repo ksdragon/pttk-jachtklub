3ae597ecd72210ad44a87acf3afc880b899bb0ec
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-09-09 13:47:56 +0200

    add mdb material bootrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { TopbarComponent } from './header/topbar/topbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -14,7 +15,8 @@ import { LogoComponent } from './header/logo/logo.component';
     TopbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
