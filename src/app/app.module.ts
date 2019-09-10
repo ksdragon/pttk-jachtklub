b5ca6398d989c505c815a04e020a4261ba4fc1a7
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-09-10 09:09:17 +0200

    styling header logo topbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LogoComponent } from './header/logo/logo.component';
+import { MainSlidershowComponent } from './extrasCopmonent/main-slidershow/main-slidershow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoComponent,
-    TopbarComponent
+    TopbarComponent,
+    MainSlidershowComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/extrasCopmonent/main-slidershow/main-slidershow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-slidershow',
+  templateUrl: './main-slidershow.component.html',
+  styleUrls: ['./main-slidershow.component.scss']
+})
+export class MainSlidershowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
