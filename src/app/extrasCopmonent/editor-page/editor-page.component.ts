92f7c4475d59265fd99b37f9dfe052a3db7cb825
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-09-20 11:52:42 +0200

    resize algin working

--- src/app/extrasCopmonent/editor-page/editor-page.component.ts
+++ src/app/extrasCopmonent/editor-page/editor-page.component.ts
@@ -1,3 +1,4 @@
+import { redo_icon } from 'quill/assets/icons/redo.svg';
 import { ImageDrop } from 'quill-image-drop-and-paste';
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import Quill from 'quill';
@@ -16,6 +17,50 @@ DividerBlot.blotName = 'divider';
 DividerBlot.tagName = 'hr';
 Quill.register(DividerBlot);
 
+// class SpanBlockClass extends BlockEmbed {}
+// SpanBlockClass.blotName = 'span-block';
+// SpanBlockClass.tagName = 'span';
+
+const Parchment = Quill.import('parchment');
+const config = { scope: Parchment.Scope.BLOCK };
+const SpanBlockClass = new Parchment.Attributor.Class('span-block', 'span', config);
+Quill.register(SpanBlockClass, true);
+
+const icons = Quill.import('ui/icons');
+icons['span-block'] = 'sb';
+
+// solution for keep style in img
+const BaseImageFormat = Quill.import('formats/image');
+const ImageFormatAttributesList = [
+    'alt',
+    'height',
+    'width',
+    'style'
+];
+
+class ImageFormat extends BaseImageFormat {
+  static formats(domNode) {
+    return ImageFormatAttributesList.reduce(function(formats, attribute) {
+      if (domNode.hasAttribute(attribute)) {
+        formats[attribute] = domNode.getAttribute(attribute);
+      }
+      return formats;
+    }, {});
+  }
+  format(name, value) {
+    if (ImageFormatAttributesList.indexOf(name) > -1) {
+      if (value) {
+        this.domNode.setAttribute(name, value);
+      } else {
+        this.domNode.removeAttribute(name);
+      }
+    } else {
+      super.format(name, value);
+    }
+  }
+}
+
+Quill.register(ImageFormat, true);
 
 @Component({
   selector: 'app-editor-page',
@@ -31,14 +76,27 @@ export class EditorPageComponent implements OnInit, AfterViewInit {
   focused = false;
   editorInstance: any;
   placeholder = 'Utwórz swój artykuł...';
+  toolbarOptions = [[ 'emoji' ], ['formula'], ['image'], ['blockquote', 'code-block', 'span-block', 'link'], ['hr']];
+
+  handlerOptions = {
+    'span-block': () => {
+      const range = this.editorInstance.getSelection();
+      const  format = this.editorInstance.getFormat(range);
+      console.log(range);
+      if (!format['span-block']) {
+        this.editorInstance.format('span-block', 'block');
+      } else {
+        this.editorInstance.removeFormat(range.index, range.index + range.length);
+      }
+    }
+  };
+
+    editorStyle = {
+    minHeight: '300px',
+    backgroundColor: '#fff'
+  };
 
   constructor() {
-    this.modules = {
-      imageResize: {},
-      imageDrop: {},
-      'emoji-toolbar': true,
-      toolbar: { container: [[ 'emoji' ], ['formula'], ['image'], ['code-block'], ['hr']]  }
-    };
    }
 
   created(event) {
@@ -53,7 +111,7 @@ export class EditorPageComponent implements OnInit, AfterViewInit {
   }
 
   ContentChanged(event) {
-    this.contentView = event.json;
+    this.contentView = event.html;
     console.log(this.contentView);
   }
 
@@ -73,23 +131,55 @@ export class EditorPageComponent implements OnInit, AfterViewInit {
 
 
   ngOnInit() {
+    this.modules = {
+      imageResize: {},
+      imageDrop: {},
+      'emoji-toolbar': true,
+      toolbar: {
+        container: this.toolbarOptions,
+        handlers: this.handlerOptions
+      }
+    };
   }
 
   ngAfterViewInit() {
-    const dividerToolbarButton = document.querySelector('.ql-hr');
-    dividerToolbarButton.textContent = '<Hr>';
-    dividerToolbarButton.classList.add('mdi');
-    dividerToolbarButton.classList.add('mdi-minus');
-    dividerToolbarButton.classList.add('mdi-18px');
-    dividerToolbarButton.addEventListener('click', (e) => {
-       e.preventDefault();
-
-       const range = this.editorInstance.getSelection(true);
-
-       // The editor_instance was created during onEditorCreated event.
-       this.editorInstance.insertText(range.index, '\n', Quill.sources.USER);
-       this.editorInstance.insertEmbed(range.index + 1, 'divider', true, Quill.sources.USER);
-       this.editorInstance.setSelection(range.index + 2, Quill.sources.SILENT);
-    });
+    this.addDivderToEditorToolbar();
+    // this.addSpanBlockToToolbar();
  }
-}
+
+ addDivderToEditorToolbar() {
+  const dividerToolbarButton = document.querySelector('.ql-hr');
+  dividerToolbarButton.textContent = '<Hr>';
+  dividerToolbarButton.classList.add('mdi');
+  dividerToolbarButton.classList.add('mdi-minus');
+  dividerToolbarButton.classList.add('mdi-18px');
+  dividerToolbarButton.addEventListener('click', (e) => {
+     e.preventDefault();
+
+     const range = this.editorInstance.getSelection(true);
+
+     // The editor_instance was created during onEditorCreated event.
+     this.editorInstance.insertText(range.index, '\n', Quill.sources.USER);
+     this.editorInstance.insertEmbed(range.index + 1, 'divider', true, Quill.sources.USER);
+     this.editorInstance.setSelection(range.index + 2, Quill.sources.SILENT);
+  });
+  }
+  addSpanBlockToToolbar() {
+    const spanBlockToolBarButton = document.querySelector('.ql-span-block');
+
+    spanBlockToolBarButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        const range = this.editorInstance.getSelection(true);
+        const format = this.editorInstance.getFormat(range);
+        console.log(range);
+        console.log(format);
+        if (!format['span-block']) {
+          // this.editorInstance.formatText(range, '.span-block');
+          this.editorInstance.format('span-block', 'block');
+        } else {
+          this.editorInstance.removeFormat(range.index, range.index + range.length);
+        }
+      }
+    );
+    }
+ }
