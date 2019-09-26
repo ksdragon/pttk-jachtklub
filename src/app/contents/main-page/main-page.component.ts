4fe05d31b3a494a481495af9c6500c11194c908d
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-09-26 13:57:27 +0200

    error with delta

--- src/app/contents/main-page/main-page.component.ts
+++ src/app/contents/main-page/main-page.component.ts
@@ -1,8 +1,9 @@
-import { Quill } from 'quill';
 import { EditorService } from './../../extrasCopmonent/editor/editor.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ArticlePage } from 'src/app/shared/article-page.model';
+// import * as Delta from 'quill-delta/dist/Delta';
+// import * as Delta from 'quill-delta';
 
 @Component({
   selector: 'app-main-page',
@@ -15,9 +16,21 @@ export class MainPageComponent implements OnInit, OnDestroy {
   articles: ArticlePage [] = [];
   articlesLayout = [];
   layoutsViews = [];
+  quill = {
+    ops: [
+      { insert: 'Gandalf', attributes: { bold: true } },
+      { insert: ' the ' },
+      { insert: 'Grey', attributes: { color: '#cccccc' } }
+    ]
+  };
+  delta = new Delta(this.quill);
+
+
   constructor(private editorService: EditorService) { }
 
   ngOnInit() {
+    console.log('OnInit main-page',  this.delta);
+    this.articlesLayout.push(this.delta);
     // this.subscription = this.editorService.articlesChanged.subscribe(
     //   (articlesPage) => {
     //     console.log(articlesPage);
